ee94fb5d79ea5ab2167c79a5006a21031025b4c6
Advanced Alpaka <advancedAlpaka@protonmail.com>
2023-08-06 17:48:06 +0300

    serialization

--- src/jvmMain/kotlin/org.example.application/plugins/Serialization.kt
+++ src/jvmMain/kotlin/org.example.application/plugins/Serialization.kt
@@ -1,7 +1,15 @@
 package org.example.application.plugins
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
-    // TODO: configure serialization
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
 }
