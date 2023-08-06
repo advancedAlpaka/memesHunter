f29bc474b63e313c8b3032c0fa05e64b84e5e876
Advanced Alpaka <advancedAlpaka@protonmail.com>
2023-08-06 17:16:18 +0300

    upgrading script

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,10 +54,33 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+                // https://ktor.io/docs/engines.html
                 implementation("io.ktor:ktor-server-netty:$ktor_version")
-                implementation("io.ktor:ktor-server-html-builder-jvm:$ktor_version")
-                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$ktx_html_version")
+
+                // https://ktor.io/docs/html-dsl.html
+                implementation("io.ktor:ktor-server-html-builder:$ktor_version")
+
+                // https://ktor.io/docs/session-auth.html
+                implementation("io.ktor:ktor-server-auth:$ktor_version")
+                implementation("io.ktor:ktor-server-sessions:$ktor_version")
+
+                // https://ktor.io/docs/kotlin-serialization.html
+                implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+
+                // https://ktor.io/docs/logging.html
+                implementation("ch.qos.logback:logback-classic:1.4.9")
+
+                // https://docs.mongodb.com/manual/administration/install-community/
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.10.1")
+                implementation("it.skrape:skrapeit:1.1.5")
+                implementation("io.github.oshai:kotlin-logging-jvm:5.0.2")
+                implementation("commons-cli:commons-cli:1.5.0")
+                implementation("commons-io:commons-io:2.11.0")
+                implementation("org.slf4j:slf4j-simple:2.0.7")
+                implementation("org.slf4j:slf4j-api:2.0.7")
+
             }
         }
         val jvmTest by getting
